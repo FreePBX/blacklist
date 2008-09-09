2532de337f6e3728d04a315f05e144b48bce6ba9
Micke Carlsson <mickecamino@gmail.com>
2008-09-09 11:02:10 +0000

    Updated swedish translation, added text string to localization for blacklist

--- install.php
+++ install.php
@@ -1,18 +1,21 @@
 <?php
+$blacklistnum = _("Blacklist a number");
 $fcc = new featurecode('blacklist', 'blacklist_add');
-$fcc->setDescription('Blacklist a number');
+$fcc->setDescription($blacklistnum);
 $fcc->setDefault('*30');
 $fcc->update();
 unset($fcc);
 
+$blacklistremove = _("Remove a number from the blacklist");
 $fcc = new featurecode('blacklist', 'blacklist_remove');
-$fcc->setDescription('Remove a number from the blacklist');
+$fcc->setDescription($blacklistremove);
 $fcc->setDefault('*31');
 $fcc->update();
 unset($fcc);
 
+$blacklistlast = _("Blacklist the last caller");
 $fcc = new featurecode('blacklist', 'blacklist_last');
-$fcc->setDescription('Blacklist the last caller');
+$fcc->setDescription($blacklistlast);
 $fcc->setDefault('*32');
 $fcc->update();
 unset($fcc);

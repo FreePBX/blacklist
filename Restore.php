720726cc22bbf18c34a1150119a9195747b01e75
James Finstrom <jfinstrom@sangoma.com>
2018-08-06 21:51:44 +0000

    Add the ability to process legacy restore

--- Restore.php
+++ Restore.php
@@ -3,12 +3,21 @@ namespace FreePBX\modules\Blacklist;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = $this->restoreObj->getConfigs();
+    $configs = $this->getConfigs();
     foreach($configs as $item){
       if(empty($item['number'])){
         continue;
       }
       $this->FreePBX->Blacklist->numberAdd($item);
+    }
+  }
+  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+    $astdb = $this->getAstDb($tmpfiledir.'/astdb');
+    if(!isset($astdb['blacklist'])){
+      return $this;
+    }
+    foreach($astdb['blacklist'] as $number => $desc){
+      $this->FreePBX->Blacklist->numberAdd(['number' => $number, 'description' => $desc]);
     }
   }
 }

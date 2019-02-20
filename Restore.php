7bea92a00a2b56f92156c7241f59fb0022d3d2f9
Andrew Nagy <andrew.nagy@the159.com>
2019-02-20 17:34:16 -0500

    [Module Tag script: blacklist 15.0.2.5] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,22 +2,21 @@
 namespace FreePBX\modules\Blacklist;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    foreach($configs as $item){
-      if(empty($item['number'])){
-        continue;
-      }
-      $this->FreePBX->Blacklist->numberAdd($item);
-    }
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $astdb = $this->getAstDb($tmpfiledir.'/astdb');
-    if(!isset($astdb['blacklist'])){
-      return $this;
-    }
-    foreach($astdb['blacklist'] as $number => $desc){
-      $this->FreePBX->Blacklist->numberAdd(['number' => $number, 'description' => $desc]);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		foreach($configs as $item){
+			if(empty($item['number'])){
+				continue;
+			}
+			$this->FreePBX->Blacklist->numberAdd($item);
+		}
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		if(!isset($astdb['blacklist'])){
+			return $this;
+		}
+		foreach($astdb['blacklist'] as $number => $desc){
+			$this->FreePBX->Blacklist->numberAdd(['number' => $number, 'description' => $desc]);
+		}
+	}
 }
